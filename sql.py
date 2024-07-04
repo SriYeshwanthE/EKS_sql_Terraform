15ca997db1e6d02255a6dcfed69f29c1235dd393
Sri Yeshwanth Ekavasalu <165659636+SriYeshwanthE@users.noreply.github.com>
2024-07-04 12:20:05 +0530

    connection for sql db

---
+++ sql.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+      host="a5374d202694b4a65b235f0abb083f83-1654998043.us-east-1.elb.amazonaws.com",
+      user="sri",
+      port="3306",
+      password="2135",
+)
+
